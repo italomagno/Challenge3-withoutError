29d5f97e57168d35be5c3fec509dea594d8235f2
Italo Magno <italomagno10@gmail.com>
2021-11-16 14:33:53 -0300

    LETSGO

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,11 +23,12 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+     const storagedCart = JSON.stringify(localStorage.getItem('@RocketShoes:cart'));
+    
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+     if (storagedCart) {
+       return JSON.parse(storagedCart);
+     }
 
     return [];
   });
